f87a04e00104782f483653102aab282a3a49da1c
James Paterni <james@ruby-code.com>
2014-03-31 19:54:45 -0400

    Messed up everything

--- app.rb
+++ app.rb
@@ -9,15 +9,32 @@ class GameWindow < Gosu::Window
     super 1024, 768, false
     self.caption = "Two ships shooting at eachother."
     @background_image = Gosu::Image.new(self, "media/Space.png", true)
+
+    ## Create Players
     @player_one = Player.new(self)
     @player_one.move(self.width - @player_one.image.width/2, self.height - @player_one.image.height/2)
     @player_two = Player.new(self)
     @player_two.move(@player_two.image.width/2,@player_two.image.height/2)
+
+    ## Arrays to hold objects
     @players = [@player_one,@player_two]
     @shots = []
     @walls = []
+
+    @things = [ @players, @shots, @walls]
+
+    ## Create Score Text
     @score_one = Gosu::Font.new(self, Gosu::default_font_name, 20)
     @score_two = Gosu::Font.new(self, Gosu::default_font_name, 20)
+
+    createWalls
+  end
+
+  def destroyed?
+    false
+  end
+
+  def createWalls
     4.times do |t|
       # Player One Walls
       wall = Wall.new(self)
@@ -30,7 +47,7 @@ class GameWindow < Gosu::Window
       wall.x = wall.image.height * 3
       @walls << wall
     end
-    #(rand(5)+5).times { @walls << Wall.new(self) }
+    #(rand(5)+5).times { @walls << Wall.new(self) }  end
   end
 
   def update
@@ -64,26 +81,27 @@ class GameWindow < Gosu::Window
       @shots << Shot.new(self,@player_two) unless
         @shots.select {|shot| shot.player == @player_two and Gosu::distance(shot.x, shot.y, shot.origin_x, shot.origin_y) < 300}.count > 0 # Don't shoot if just shot
     end
+    
+    detect_colission
 
-    ## Handle shooting
-    @shots.reject! { |shot|
-        (shot.x > 1024 or shot.y > 768) or (shot.x < 0 or shot.y < 0)  or
-        @walls.select { |wall|
-          Gosu::distance(shot.x, shot.y, wall.x, wall.y) < wall.image.width
-        }.count > 0
+    # Remove objects that no longer exist
+    @things.each {|thing| thing.delete_if {|thing| thing.destroyed } }
 
-    } # Remove shots that are off screen
-    @shots.each { |shot|
-      ## Handle collision detection.
-      @players.each {|player|
-        if player != shot.player and Gosu::distance(shot.x, shot.y, player.x, player.y) < shot.image.height/2
-          shot.player.score += 1  # Increase shooters score
-          player.die  # make player respaon
-          @shots -= [shot] # Remove shot from screen.
-        end
+    # Handle border collision
+#    @things.flatten.each {|thing| thing.collide(self) if thing.x >= self.width - thing.image.width / 2 or thing.x <= 0 + thing.image.width or thing.y >= self.height - thing.image.height or thing.y <= 0 + thing.image.height }
+
+    # Update all the things.
+    @things.flatten.each {|thing| thing.update}
+
+  end
+
+  def detect_colission
+    @things.flatten.each {|thing|
+      @things.flatten.each { |other_thing|
+        thing.collide(other_thing) if thing != other_thing and Gosu::distance(thing.x,thing.y,other_thing.x,other_thing.y) < thing.image.height
+        }
       }
-      shot.update
-    }
+    @things.flatten.each {|thing| thing.collide(self) if thing.x > self.width or thing.y > self.height or thing.x < 0 or thing.y < 0 }
   end
 
   def draw
@@ -106,17 +124,39 @@ end
 
 ## Basic Game Object will be inherited by other objects
 class GameObject
-  attr_accessor :score, :x, :y
-  attr_reader :rotation, :z, :image
+  attr_accessor :score, :x, :y, :destroyable
+  attr_reader :rotation, :z, :image, :destroyed, :speed
 
   def initialize(window,image_file,x=0,y=0,z=0)
     @window = window
     @x ||= x; @y ||= y; @z ||= z
     @rotation ||= 0
     @image = Gosu::Image.new(window, image_file, false)
+    @destroyed = false
+    @destroyable = true
+    @speed = 0
+  end
+
+  def corners
+    # 0     2
+    # .-----,
+    # |     |
+    # |     |
+    # `-----'
+    # 1     3
+    [@x-image.width/2,@y-image.height/2,@x+image.width/2,@y+image.height/2]
+  end
+
+  def destroy
+    @destroyed = true
   end
 
   def update
+  end
+
+  def collide(obj)
+    puts "#{self.class} collided with #{obj.class}"
+    destroy if obj === Shot and Shot.player != self
   end
 
   def move(x,y,rotation=nil)
@@ -153,15 +193,27 @@ class Player < GameObject
   def turn_left
     @rotation -= 3
   end
-  def forward
-    @x += Gosu::offset_x(@rotation, 5) unless (@x + @image.width/2 + Gosu::offset_x(@rotation, 5) > @window.width or
-      @x - @image.width/2 + Gosu::offset_x(@rotation, 5) < 0) or
-      @window.walls.select {|wall| Gosu::distance(Gosu::offset_x(@rotation, 5), Gosu::offset_y(@rotation, 5), wall.x, wall.y) < wall.image.height/2 }.count > 0
-    @y += Gosu::offset_y(@rotation, 5) unless (@y + @image.height/2 + Gosu::offset_y(@rotation, 5) > @window.height or
-      @y - @image.height/2 + Gosu::offset_y(@rotation, 5) < 0) or
-      @window.walls.select {|wall| Gosu::distance(Gosu::offset_x(@rotation, 5), Gosu::offset_y(@rotation, 5), wall.x, wall.y) < wall.image.height/2 }.count > 0
+
+  def collide(obj)
+    puts "player #{obj.object_id} collided with #{obj.class}"
+    case obj
+    when Shot
+      destroy unless obj.player == self
+    else
+      @speed = (@speed + obj.speed) * -0.5
+    end
   end
-  def die
+
+  def update
+    @x += Gosu::offset_x(@rotation, @speed)
+    @y += Gosu::offset_y(@rotation, @speed)
+    @speed -= 0.2 if @speed > 0
+  end
+
+  def forward
+    @speed += 1 if @speed < 10
+  end
+  def destroy
     # Respawn in random location
     @death_sound.play
     move(rand(@window.width-@image.width)+@image.width,rand(@window.height-@image.height)+@image.height,rand(360))
@@ -178,14 +230,19 @@ class Shot < GameObject
     @origin_y = @player.y
     @rotation = @player.rotation
     @z = 1
+    @speed = 10
     super window,"media/Shot.bmp", @origin_x, @origin_y
     @sound.play
   end
 
-## Make shots move across screen
+  def collide(obj)
+    destroy unless obj == @player
+  end
+
+  ## Make shots move across screen
   def update
-    @x += Gosu::offset_x(@rotation, 10)
-    @y += Gosu::offset_y(@rotation, 10)
+    @x += Gosu::offset_x(@rotation, @speed)
+    @y += Gosu::offset_y(@rotation, @speed)
   end
 end
 
